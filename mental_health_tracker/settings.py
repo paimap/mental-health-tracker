9623f01cc2f0723c4f3cb97255e2fb23c7a4851a
Paima Ishak Melkisedek Purba <paima@Paimas-MacBook-Pro.local>
2024-09-11 16:08:13 +0700

    add

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://paima-ishak-mentalhealthtracker1.pbp.cs.ui.ac.id", "https://http://paima-ishak-mentalhealthtracker1.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://paima-ishak-mentalhealthtracker1.pbp.cs.ui.ac.id", "https://paima-ishak-mentalhealthtracker1.pbp.cs.ui.ac.id"]
