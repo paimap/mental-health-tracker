5fd2edff54c9c76b0bb0a257eb4ca357c7bea83b
Paima Ishak Melkisedek Purba <paima@Paimas-MacBook-Pro.local>
2024-09-25 15:10:25 +0700

    add tugas

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register
 from main.views import login_user, logout_user
+from main.views import edit_mood
+from main.views import delete_mood
 
 app_name = 'main'
 
@@ -11,4 +13,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'),
 ]
