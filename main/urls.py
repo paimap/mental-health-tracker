b0c931087848538e9b89ee81626b64758d5c03fb
Paima Ishak Melkisedek Purba <paima@Paimas-MacBook-Pro.local>
2024-10-02 15:43:02 +0700

    add

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import show_main, create_mood_entry, show_xml, show_json, show_x
 from main.views import register
 from main.views import login_user, logout_user
 from main.views import edit_mood
-from main.views import delete_mood
+from main.views import delete_mood, add_mood_entry_ajax
 
 app_name = 'main'
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'),
+    path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
 ]
