cab56e33a8d05b768556631807cfb141b4b531f3
Paima <paimapurba@gmail.com>
2024-11-13 16:18:15 +0700

    tutor 8

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class MoodEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,16 @@ from main.views import show_main, create_mood_entry, show_xml, show_json, show_x
 from main.views import register
 from main.views import login_user, logout_user
 from main.views import edit_mood
-from main.views import delete_mood, add_mood_entry_ajax
+from main.views import delete_mood, add_mood_entry_ajax, create_mood_flutter
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'), path('create-mood-entry', create_mood_entry, name='create_mood_entry'),path('xml/', show_xml, name='show_xml'),path('json/', show_json, name='show_json'),path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('', show_main, name='show_main'), 
+    path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
@@ -16,4 +20,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'),
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
